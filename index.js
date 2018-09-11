f15404ad4802a2ba389697338e9db7bf40362731
jilucev <jilucev7@gmail.com>
2018-09-11 11:48:11 -0700

    Adds pr webhook events

--- index.js
+++ index.js
@@ -11,9 +11,28 @@ module.exports = app => {
     return context.github.issues.createComment(issueComment)
   })
 
+  app.on('pull_request.opened', async context => {
+    app.log('pr was opened')
+  })
+
+  app.on('pull_request.reopened', async context => {
+    app.log('pr was reopened')
+  })
+
+  app.on('pull_request.edited', async context => {
+    app.log('pr was edited')
+  })
+
+  app.on('pull_request.closed', async context => {
+    app.log('pr was closed')
+  })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 }
+// Most events also include an "action".
+// For example, the issues event has actions of
+// assigned, unassigned, labeled, unlabeled, opened, edited, milestoned, demilestoned, closed, and reopened
