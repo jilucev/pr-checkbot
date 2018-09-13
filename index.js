f04b0beea8e4597d3d20c6a4e9d80ddbc13a6814
jilucev <jilucev7@gmail.com>
2018-09-13 16:58:33 -0700

    Adds finesse

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ module.exports = app => {
     return context.github.issues.createComment(issueComment);
   })
 
-  //cleans up the comments when the pr is deleted
-  // assigned, unassigned, labeled, unlabeled, opened, edited, closed, reopened, or synchronized
+  // cleans up the comments when the pr is deleted
+  // If the pull request is closed because it's just been merged,
+  // Gets the merge commit and checklist body and feeds them to updateChangeLog
   app.on('pull_request.closed', async context => {
     app.log(`pull request was closed`);
     const params = context.issue();
@@ -24,23 +25,23 @@ module.exports = app => {
 
     const comments = await context.github.issues.getComments({owner, repo, number});
     const checklistComment = comments.data.filter(comment => comment.body.includes(`# Code Review Checklist`));
-    const checklist = checklistComment[0].body;
-    const checklistParams = context.issue({ body: checklist});
+    const checklistBody = checklistComment[0].body;
+    const checklistParams = context.issue({ body: checklistBody});
 
-    app.log(`checklist body in pr closed ${checklist}`);
-
+    //Get merge commit and use data in changelog
     let mergeCommit;
-
-    const events = (await context.github.activity.getEventsForRepo({owner, repo}).then(events => {
-      mergeCommit = events.data[0].payload.commits.pop();
-      app.log(`merge commit ${JSON.stringify(mergeCommit, null, 4)}`);
-    }));
 
     const mergeCheck = await context.github.pullRequests.checkMerged({owner, repo, number}).catch(e => app.log(` Error with merge check ${e}`));
 
-    if(mergeCheck.status == 204) {
+    if(!!mergeCheck && mergeCheck.status == 204) {
       app.log('was merged!')
-      updateChangeLog(context, checklist, mergeCommit);
+
+      const events = (await context.github.activity.getEventsForRepo({owner, repo}).then(events => {
+        mergeCommit = events.data[0].payload.commits.pop();
+        app.log(`merge commit ${JSON.stringify(mergeCommit, null, 4)}`);
+      }));
+
+      updateChangeLog(context, checklistBody, mergeCommit);
       context.github.issues.createComment(checklistParams);
     }
 
@@ -50,12 +51,10 @@ module.exports = app => {
   })
 
   app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.edited'], async context => {
-    // Return the owner, repo, and number params for making API requests against an issue or pull request.
-    // The object passed in will be merged with the repo params.
-
     const params = context.issue();
     const {owner, repo, number} = params;
 
+    //Load the checklist rules from the repo. Must be located at '.github/PR_CHECKLIST.json'
     const rulesFile = await context.github.repos.getContent({owner, repo, path: '.github/PR_CHECKLIST.json'});
     const rules = JSON.parse(Buffer.from(rulesFile.data.content, 'base64').toString()).rules;
 
@@ -69,6 +68,7 @@ module.exports = app => {
 
    const messages = [ '# Code Review Checklist' ];
 
+   //Use the rules file to build the checklist
    matchingRules.forEach(rule => {
      messages.push('## ' + rule.name);
 
@@ -81,10 +81,12 @@ module.exports = app => {
 
     const sha = context.payload.pull_request.head.sha;
 
+    // Add the checklist as a comment on the PR
     return context.github.repos
       .createStatus({ owner, repo, sha, state: 'pending', context: 'Code Review', description: 'Checklist' });
   })
 
+  // Build the new changelog content and commit it to the repo's changelog. Must be located at '.github/release_changelog.md'
   async function updateChangeLog(context, checklist, mergeCommit) {
     app.log(`checklist in updateChangeLog ${checklist}`);
     const {owner, repo, number} = context.issue();
@@ -94,20 +96,20 @@ module.exports = app => {
     const rootSha = lastCommit.sha;
 
     const authorInfo = lastCommit.commit.author;
-
-    // TODO: How to get this?
-    const mergeCommitMessage = lastCommit.message || 'no message';
+    const mergeCommitMessage = lastCommit.message || 'no message present on merge commit';
 
     const changelogFile = await context.github.repos.getContent({owner, repo, path: '.github/release_changelog.md'});
     const changelogFileContents = Buffer.from(changelogFile.data.content, 'base64').toString();
 
     let info = [
-      mergeCommit.sha,
-      mergeCommit.author.email,
-      mergeCommit.message,
+      ` ${'='.repeat(80)} `,
+      ` ${mergeCommit.author.email} `,
+      ` ${mergeCommit.message} `,
+      ` ${mergeCommit.sha}`,
       checklist,
-      changelogFileContents
-    ].join('/n');
+      changelogFileContents,
+      ` ${'='.repeat(80)} `
+    ].join('\n');
 
     const content = Buffer.from(info).toString('base64');
 
@@ -116,7 +118,7 @@ module.exports = app => {
         owner: owner,
         repo: repo,
         path: '.github/release_changelog.md',
-        message: "This is a pony changelog",
+        message: mergeCommit.message,
         content: content,
         sha: changelogFile.data.sha
       }
@@ -125,7 +127,7 @@ module.exports = app => {
     const commitParams = {
       owner,
       repo,
-      message: 'Ponyfriends test',
+      message: mergeCommit.message,
       tree: rootSha
     }
 
@@ -142,19 +144,29 @@ module.exports = app => {
 
         const checklistComplete = !body.includes('[ ]');
 
-        const message = [];
-        const image = `!['I like what you did there'](https://media.giphy.com/media/10PixLlze8fYiI/giphy.gif "I like what you did there")`
-
-        message.push(`# Go for it.`);
-        message.push(image);
-
-        const params = context.issue({body: message.join('')});
-
         if (checklistComplete) {
+
           app.log('checklist complete');
-          //also need to update the file and commit
-          // updateChangeLog(context, body);
-          // context.github.issues.createComment(params);
+          const message = [];
+          const image = `!['I like what you did there'](https://media.giphy.com/media/10PixLlze8fYiI/giphy.gif "I like what you did there")`
+
+          message.push(`# Go for it.`);
+          message.push(image);
+          const params = context.issue({body: message.join('')});
+          context.github.issues.createComment(params);
+
+          // Set the checklist status to green
+          const pr = await context.github.pullRequests.get({owner, repo, number});
+          const sha = pr.data.head.sha;
+          context.github.repos.createStatus({
+            owner,
+            repo,
+            sha,
+            state: checklistComplete ? 'success' : 'pending',
+            context: 'Code Review',
+            description: 'Checklist'
+          });
+
         } else {
           // Remove the gif if the checklist is no longer complete
           const comments = await context.github.issues.getComments({owner, repo, number});
@@ -162,18 +174,6 @@ module.exports = app => {
 
           return gifComments.forEach(gifComment => context.github.issues.deleteComment({owner, repo, comment_id: gifComment.id}));
         }
-
-        // Set the checklist status to green
-        const pr = await context.github.pullRequests.get({owner, repo, number});
-        const sha = pr.data.head.sha;
-        context.github.repos.createStatus({
-          owner,
-          repo,
-          sha,
-          state: checklistComplete ? 'success' : 'pending',
-          context: 'Code Review',
-          description: 'Checklist'
-        });
       }
     }
   });
